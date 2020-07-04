ca2c57d42ed85541133614a7920e21c150790c0d
swagatikadd1995 <swagatikadd1995@gmail.com>
2020-07-04 12:31:57 +0530

    Created the Project with Basic Dependensies

---
+++ src/main/java/com/simpragma/management/TradesManagementApplication.java
@@ -0,0 +1,13 @@
+package com.simpragma.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradesManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradesManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simpragma/management/TradesManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpragma.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradesManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
